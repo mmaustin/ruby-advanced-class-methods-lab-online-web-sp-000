91e926aa0f1ff6971f9a6ce2c94b443972000055
mccray austin <tubach75@hotmail.com>
2020-10-12 14:43:22 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -7,7 +7,53 @@ class Song
   end
 
   def save
-    self.class.all << self
+    self.class.all << self #seems like this refers to an instance and not the class
+  end
+
+  def self.create
+    song = self.new
+    @@all << song
+    song
+  end
+
+  def self.new_by_name(name)
+    song = self.new
+    song.name = name
+    song
+  end
+
+  def self.create_by_name(name)
+    song = self.new
+    song.name = name
+    @@all << song
+    song
+  end
+
+  def self.find_by_name(name)
+    @@all.find {|tune| tune.name == name}
+=begin
+    if !@@all.find {|song| song.name == name}
+      false
+    else
+      @@all.find {|song| song.name == name}
+    end
+=end
+  end
+
+  def self.find_or_create_by_name(name)
+    if self.find_by_name(name) == nil
+      self.create_by_name(name)
+    else
+      self.find_by_name(name)
+    end
+  end
+
+  def self.alphabetical
+    array = []
+    @@all.each do |tune|
+      array << tune.name
+    end
+    array.sort
   end
 
 end
